519d4fb693fb201a1680cd700802752bae6bf29b
Karthik Purushothaman <karthik.purushothaman@gmail.com>
2021-02-24 20:58:28 +0530

    more comments

--- DAL/DataAccess.Interface/IProductRepository.cs
+++ DAL/DataAccess.Interface/IProductRepository.cs
@@ -2,9 +2,39 @@
 
 namespace DataAccess.Interface
 {
+    //public abstract class ProductRepository
+    //{
+    //    public virtual Tuple<string, double> GetProductInfo() 
+    //    { 
+    //        return new Tuple<string, double>("dummy", 0.0); 
+    //    }
+    //    public abstract void SetProduct(Tuple<string, double> product);
+    //}
+
+    //public class CloudRepo : ProductRepository
+    //{
+    //    public override Tuple<string, double> GetProductInfo()
+    //    {
+    //        return base.GetProductInfo();
+    //    }
+    //    public override void SetProduct(Tuple<string, double> product)
+    //    {
+    //        //implementation
+    //    }
+    //}
+
+    //ProductService -> SqlRepo
+    //      |
+	//      V
+
+    //ProductSerice -> Data.Interface<- SqlRepo
+    //            <- FileRepo
+    //            <- CloudRepo
+
+
     public interface IProductRepository
     {
-        Tuple<string,double> GetProductInfo();
+        Tuple<string, double> GetProductInfo();
         void SetProduct(Tuple<string, double> product);
     }
 }
